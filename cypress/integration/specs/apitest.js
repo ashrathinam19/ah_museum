fd8bb9d03bc56df661b944b86f6371fcdd9f8d80
Ashok <ashokrathinam19@gmail.com>
2023-01-16 01:37:05 +0530

    Initial commit..

--- cypress/integration/specs/apitest.js
+++ cypress/integration/specs/apitest.js
@@ -1,7 +1,8 @@
 describe('Verifies Posts',()=>{ 
     it("basic_check",() =>{
-        cy.request('https://www.rijksmuseum.nl/api/nl/collection/SK-C-5?key=0fiuZFh4').then((response)=>{
+        cy.request('https://www.rijksmuseum.nl/api/nl/collection?key=0fiuZFh4').then((response)=>{
             expect(response.status).to.eq(200)     
     })
+    it("")
 })
 })
